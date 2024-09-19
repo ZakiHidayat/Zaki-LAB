5e8cb74bdfd231811a3459d4db0dcf141a576d89
Zaki Hidayat <zakihidayat260@gmail.com>
2024-09-19 18:35:19 +0700

    Update

---
+++ session2.py
@@ -0,0 +1,76 @@
+# -------------- Operator Aritmatika -------------- 
+
+x = 9
+y = 2
+ 
+#penjumlahan
+print("x + y = ", x+y)
+
+#penguragan
+print("x - y = ", x-y)
+
+#perkalian
+print("x x y = ", x*y)
+
+#pembagian
+print("x / y =", x//y)
+
+#modulus
+print("x % y = ", x%y)
+
+#pangkat
+print("x ** y = ", x**y)
+
+
+# -------------- Operator Assignment -------------- 
+
+a = 2
+
+a += 4
+
+a -= 1
+
+a *= 10
+
+a /= 5
+
+a **= 2
+
+print(a)
+
+# a dikali 10, dibagi 5, pangkat 2
+
+
+# -------------- Operator Relational -------------- 
+
+x = 5 
+y = 4
+
+print("x > y = ", x>y)
+print("x < y = ", x<y)
+print("x == y = ", x==y)
+print("x ! y = ", x!=y)
+print("x >= y = ", x>=y)
+print("x <= y = ", x<=y)
+
+
+# -------------- Operator Logika --------------
+
+# and : mengembalikan True ketika kedua pernyataan benar
+# or : mengembalikan True ketika salah satu pernyataannya benar
+# not : menegasikan 
+
+x = True
+y = False
+
+#Logika AND
+a = x and y
+print(a)
+
+b = x or y
+print(b)
+
+
+# -------------- Coba-coba --------------
+
+
